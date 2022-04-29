2539f3b4d446c38cdc81bfb9c4b06bf88453e1ae
JoaoPedroCorrea <joaocorrea666@gmail.com>
2022-04-29 14:28:30 -0300

    add create user page and edit profile

--- auth_process.php
+++ auth_process.php
@@ -7,6 +7,8 @@ require_once("models/message.php");
 require_once("dao/UserDAO.php");
 
 $message = new Message($BASE_URL);
+$userDao = new UserDAO($conn, $BASE_URL);
+
 
 $type = filter_input(INPUT_POST, "type");
 
@@ -20,7 +22,39 @@ if($type === "register") {
     
     if($name && $lastname && $email && $password){
 
+        if($password === $confirmpassword){
 
+            if($userDao->findByEmail($email) === false){
+
+                $user = new User();
+
+                $userToken = $user->generateToken();
+                $finalPassword = $user->generatePassword($password);
+
+                $user->name = $name;
+                $user->lastname = $lastname;
+                $user->email = $email;
+                $user->password = $finalPassword;
+                $user->token = $userToken;
+
+                $auth = true;
+ 
+                $userDao->create($user, $auth);
+                
+                $message->setMessage("Usuário criado com sucesso.", "success", "editprofile.php");
+
+
+            } else {
+
+                $message->setMessage("Usuário já cadastrado.", "error", "back");
+
+            }
+
+        } else {
+
+            $message->setMessage("As senhas não coincidem.", "error", "back");
+
+        }
 
     } else {
         
--- models/message.php
+++ models/message.php
@@ -11,11 +11,10 @@ class Message {
 
         $_SESSION["msg"] = $msg;
         $_SESSION["type"] = $type;
-
         if($redirect != "back"){
             header("Location: $this->url" . $redirect);
         } else {
-            header("Location: " . $_SERVER["HTTP_REFERER"]);
+            header("Location: " . $this->url ."editprofile.php");
         }
 
     }
